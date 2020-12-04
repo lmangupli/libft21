d899b9ba304407ed1665c88d198c258104068373
Myesha Spyke <mspyke@va-a1.kzn.21-school.ru>
2020-12-04 20:06:45 +0300

    update libft

--- ft_strrev.c
+++ ft_strrev.c
@@ -6,7 +6,7 @@
 /*   By: mspyke <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 19:04:35 by mspyke            #+#    #+#             */
-/*   Updated: 2020/11/04 17:54:57 by mspyke           ###   ########.fr       */
+/*   Updated: 2020/12/04 20:05:54 by mspyke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,17 @@ char	*ft_strrev(char *str)
 	char	tmp;
 
 	i = 0;
-	len = ft_strlen(str);
-	while (len - 1 > i)
+	if (str)
 	{
-		tmp = str[i];
-		str[i] = str[len - 1];
-		str[len - 1] = tmp;
-		len--;
-		i++;
+		len = ft_strlen(str);
+		while (len - 1 > i)
+		{
+			tmp = str[i];
+			str[i] = str[len - 1];
+			str[len - 1] = tmp;
+			len--;
+			i++;
+		}
 	}
 	return (str);
 }
